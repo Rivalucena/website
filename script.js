6ac933767c0ff59f18e9bbb475b395e9972b903f
rivaslucena <rivasaraujo@outlook.com>
2023-03-26 08:33:07 -0300

    style: media

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const NavMenu = document.querySelector('.nav-menu');
 
 menu.addEventListener('click', ()=> {
     menu.classList.toggle('ativo');
-    menu.classList.toggle('ativo');
+    NavMenu.classList.toggle('ativo');
 })
